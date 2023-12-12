200e864148934297efc10b938234de72b6688d74
megahomyak <g.megahomyak@gmail.com>
2023-12-13 00:41:54 +0500

    Why the fuck does it freeze???

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,38 @@
-pub enum SubstitutionResult {
-    Success(String),
+enum SubstitutionResult {
+    Success {
+        new_program: String,
+        new_rule: Option<Rule>,
+    },
     InputNotMatched,
 }
 
-pub enum Rule<'a> {
+pub struct ReplacementResult {
+    pub new_rule: Option<Rule>,
+    pub substitution: String,
+}
+
+pub trait Replacer {
+    fn replace(&self, captures: &regex::Captures) -> ReplacementResult;
+}
+
+impl<T: Fn(&regex::Captures) -> ReplacementResult> Replacer for T {
+    fn replace(&self, captures: &regex::Captures) -> ReplacementResult {
+        self(captures)
+    }
+}
+
+pub enum Rule {
     Regex {
         pattern: regex::Regex,
         replacement: String,
     },
-    Builtin(&'a dyn Fn(&str) -> SubstitutionResult),
+    Builtin {
+        pattern: regex::Regex,
+        replacer: Box<dyn Replacer>,
+    },
 }
 
-impl Rule<'_> {
+impl Rule {
     fn process(&self, input: &str) -> SubstitutionResult {
         match self {
             Self::Regex {
@@ -19,33 +40,68 @@ impl Rule<'_> {
                 replacement,
             } => {
                 if pattern.is_match(input) {
-                    SubstitutionResult::Success(pattern.replace(input, replacement).to_string())
+                    SubstitutionResult::Success {
+                        new_program: pattern.replace(input, &replacement[..]).to_string(),
+                        new_rule: None,
+                    }
                 } else {
                     SubstitutionResult::InputNotMatched
                 }
             }
-            Self::Builtin(function) => function(input),
+            Self::Builtin { pattern, replacer } => match pattern.captures(input) {
+                None => SubstitutionResult::InputNotMatched,
+                Some(captures) => {
+                    let execution_result = replacer.replace(&captures);
+                    let new_program = function_string_builder::build(|mut collector| {
+                        collector.collect(&input[..captures.get(0).unwrap().start()]);
+                        collector.collect(&execution_result.substitution);
+                        collector.collect(&input[captures.get(0).unwrap().end()..]);
+                    });
+                    SubstitutionResult::Success {
+                        new_program,
+                        new_rule: execution_result.new_rule,
+                    }
+                }
+            },
         }
     }
 }
 
-pub fn execute(mut program: String, rules: &[Rule]) -> String {
+pub struct ExecutionContext {
+    pub program: String,
+    pub rules: Vec<Rule>,
+}
+
+enum MatchingStatus {
+    NoneMatched,
+    Matched { new_rule: Option<Rule> },
+}
+
+pub fn execute(context: &mut ExecutionContext) {
     loop {
-        let mut at_least_one_matched = false;
-        for rule in rules {
-            match rule.process(&program) {
-                SubstitutionResult::Success(replacement) => {
-                    at_least_one_matched = true;
-                    program = replacement;
+        let mut match_status = MatchingStatus::NoneMatched;
+        for rule in context.rules.iter().rev() {
+            match rule.process(&context.program) {
+                SubstitutionResult::Success {
+                    new_rule,
+                    new_program,
+                } => {
+                    match_status = MatchingStatus::Matched { new_rule };
+                    context.program = new_program;
+                    break;
                 }
                 SubstitutionResult::InputNotMatched => (),
             }
         }
-        if !at_least_one_matched {
-            break;
+        match match_status {
+            MatchingStatus::Matched { new_rule } => {
+                if let Some(new_rule) = new_rule {
+                    context.rules.push(new_rule)
+                }
+            }
+            MatchingStatus::NoneMatched => break,
         }
     }
-    program
 }
 
 #[cfg(test)]
@@ -54,16 +110,47 @@ mod tests {
     use regex::Regex;
 
     #[test]
-    fn it_works() {
-        assert_eq!(
-            execute(
-                "Hello!".into(),
-                &[Rule::Regex {
-                    pattern: Regex::new("ello").unwrap(),
-                    replacement: "i".into()
-                }]
-            ),
-            "Hi!"
-        )
+    fn hello() {
+        let mut context = ExecutionContext {
+            rules: [Rule::Regex {
+                pattern: Regex::new(r"ello").unwrap(),
+                replacement: "i".into(),
+            }]
+            .into(),
+            program: "Hello!".into(),
+        };
+        execute(&mut context);
+        assert_eq!(context.program, "Hi!");
+    }
+
+    #[test]
+    fn countdown() {
+        let mut context = ExecutionContext {
+            rules: [
+                Rule::Builtin {
+                    pattern: Regex::new(r"(minus \d+ \d+)").unwrap(),
+                    replacer: Box::new(|captures: &regex::Captures| {
+                        let minuend: u64 = captures.get(1).unwrap().as_str().parse().unwrap();
+                        let subtrahend: u64 = captures.get(2).unwrap().as_str().parse().unwrap();
+                        ReplacementResult {
+                            new_rule: None,
+                            substitution: format!("{}", minuend - subtrahend),
+                        }
+                    }),
+                },
+                Rule::Regex {
+                    pattern: Regex::new(r"(countdown \d+)").unwrap(),
+                    replacement: "$1 (countdown (minus $1 1))".into(),
+                },
+                Rule::Regex {
+                    pattern: Regex::new(r"(countdown 0)").unwrap(),
+                    replacement: "".into(),
+                },
+            ]
+            .into(),
+            program: "(countdown 5)".into(),
+        };
+        execute(&mut context);
+        assert_eq!(context.program, "Hi!");
     }
 }
