ba7bf72051885c2a70b4cd22e5d56e4fc0e6fe0a
megahomyak <g.megahomyak@gmail.com>
2023-12-24 15:15:14 +0500

    Trying over and over again

--- src/lib.rs
+++ src/lib.rs
@@ -31,18 +31,20 @@ pub struct FunctionResult {
     pub context_actions: Vec<ContextAction>,
 }
 
-pub struct Parameters<'a> {
-    values: Vec<&'a str>,
+pub struct Parameters {
+    values: Vec<String>,
 }
 
-impl<'a> Parameters<'a> {
+impl Parameters {
     fn get(&self, index: usize) -> &str {
-        self.values.get(index).unwrap_or(&"")
+        self.values.get(index).map(|s| &s[..]).unwrap_or("")
     }
 }
 
 pub enum Substitution {
-    String { replacement: Program<ReplacementPart> },
+    String {
+        replacement: Program<ReplacementPart>,
+    },
     Function(Box<dyn FnMut(&Parameters) -> FunctionResult>),
 }
 
@@ -58,191 +60,26 @@ impl Program<ProgramPart> {
                 ProgramPart::String(string) => result.push_str(&string),
                 ProgramPart::ProgramSequence(program_sequence) => {
                     let name = program_sequence.first_part.execute(executor);
-                    let parameters = Parameters { values:  }
+                    let parameters = Parameters {
+                        values: program_sequence
+                            .rest
+                            .into_iter()
+                            .map(|part| part.execute(executor))
+                            .collect(),
+                    };
                     if let Some(substitution) = executor.substitutions.get(name) {
-                        match substitution {
-                            Substitution::Function(function) => function()
-                        }
+                        result.push_str(&match substitution {
+                            Substitution::Function(function) => {
+                                let result = function(&parameters);
+                                result.substitution
+                            },
+                        })
                     } else {
                         result.push_str(substitution)
                     }
                 }
             }
         }
-    }
-}
-
-// --------------------------------- THIS IS OLD VVVVVVVVVVVVVVVVVVV -------------------
-impl Executor {
-    pub fn execute(&mut self, mut program: String) -> String {
-        'cycle: loop {
-            let mut chars = program.char_indices();
-            program = 'substitution: loop {
-                let mut opening_index = None;
-                let mut parts = Vec::new();
-                let mut last_string_part = String::new();
-                while let Some((current_index, c)) = chars.next() {
-                    match c {
-                        '(' => {
-                            parts.clear();
-                            last_string_part.clear();
-                            opening_index = Some(current_index)
-                        }
-                        '\\' => match chars.next() {
-                            Some((_index, escaped_c)) => {
-                                if !matches!(escaped_c, '\\' | '(' | ';' | ')') {
-                                    last_string_part.push(c);
-                                }
-                                last_string_part.push(escaped_c);
-                            }
-                            None => break 'cycle,
-                        },
-                        ';' => {
-                            last_string_part.shrink_to_fit();
-                            parts.push(last_string_part);
-                            last_string_part = String::new();
-                        }
-                        ')' => {
-                            last_string_part.shrink_to_fit();
-                            parts.push(last_string_part);
-                            if let Some(opening_index) = opening_index {
-                                let mut parts_iter = parts.iter().map(|part| &part[..]);
-                                let name = parts_iter.next().unwrap_or("");
-                                let Some(substitution) = self.substitutions.get_mut(name) else {
-                                    continue 'substitution;
-                                };
-                                let parameters = Parameters {
-                                    values: parts_iter.collect(),
-                                };
-                                let substitution = match substitution {
-                                    Substitution::String { replacement } => {
-                                        let mut chars = replacement.char_indices();
-                                        let mut substitution = String::new();
-                                        let mut opening_index = None;
-                                        let mut key_name = String::new();
-                                        while let Some((current_index, c)) = chars.next() {
-                                            match c {
-                                                '$' => match opening_index {
-                                                    None => opening_index = Some(current_index),
-                                                    Some(_) => {
-                                                        opening_index = None;
-                                                        if let Ok(index) = key_name.parse() {
-                                                            substitution
-                                                                .push_str(parameters.get(index));
-                                                        };
-                                                    }
-                                                },
-                                                '\\' => match chars.next() {
-                                                    Some((_current_index, escaped_c)) => {
-                                                        if let Some(_) = opening_index {
-                                                            if !matches!(escaped_c, '$' | '\\') {
-                                                                key_name.push(c);
-                                                            }
-                                                            key_name.push(escaped_c);
-                                                        }
-                                                    }
-                                                    None => substitution.push(c),
-                                                },
-                                                _ => substitution.push(c),
-                                            }
-                                        }
-                                        substitution
-                                    }
-                                    Substitution::Function(function) => {
-                                        let result = function(&parameters);
-                                        for action in result.context_actions {
-                                            match action {
-                                                ContextAction::AddSubstitution {
-                                                    name,
-                                                    content,
-                                                } => {
-                                                    self.substitutions.insert(name, content);
-                                                }
-                                            }
-                                        }
-                                        result.substitution
-                                    }
-                                };
-                                break 'substitution format!(
-                                    "{}{}{}",
-                                    &program[..opening_index],
-                                    substitution,
-                                    chars.as_str(),
-                                );
-                            }
-                        }
-                        c => {
-                            if let Some(_) = opening_index {
-                                last_string_part.push(c)
-                            }
-                        }
-                    }
-                }
-                break 'cycle;
-            }
-        }
-        program
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use regex::Regex;
-
-    #[test]
-    fn hello() {
-        let mut context = ExecutionContext {
-            rules: [Rule::Regex {
-                pattern: Regex::new(r"ello").unwrap(),
-                replacement: "i".into(),
-            }]
-            .into(),
-        };
-        assert_eq!(execute(&mut context, "Hello!".into()), "Hi!");
-    }
-
-    #[test]
-    fn countdown() {
-        let mut context = ExecutionContext {
-            rules: [
-                Rule::Builtin {
-                    pattern: Regex::new(r"\(minus (\d+) (\d+)\)").unwrap(),
-                    replacer: Box::new(|captures: &regex::Captures| {
-                        let minuend: usize = captures.get(1).unwrap().as_str().parse().unwrap();
-                        let subtrahend: usize = captures.get(2).unwrap().as_str().parse().unwrap();
-                        ReplacementResult {
-                            new_rule: None,
-                            substitution: format!("{}", minuend - subtrahend),
-                        }
-                    }),
-                },
-                Rule::Regex {
-                    pattern: Regex::new(r"\(countdown (\d+)\)").unwrap(),
-                    replacement: "$1 (countdown (minus $1 1))".into(),
-                },
-                Rule::Regex {
-                    pattern: Regex::new(r"\(countdown 1\)").unwrap(),
-                    replacement: "1".into(),
-                },
-            ]
-            .into(),
-        };
-        assert_eq!(execute(&mut context, "(countdown 5)".into()), "5 4 3 2 1");
-    }
-
-    #[test]
-    fn no_matches() {
-        let mut context = ExecutionContext {
-            rules: [Rule::Regex {
-                pattern: Regex::new(r"abc").unwrap(),
-                replacement: "def".into(),
-            }]
-            .into(),
-        };
-        assert_eq!(
-            execute(&mut context, "Hello, world!".into()),
-            "Hello, world!"
-        );
+        result
     }
 }
